9ac119981f368f6a5334a5392a2d9c1ce2ef6345
cc666801 <cc666801@gmail.com>
2023-03-01 16:23:36 +0800

    Update homework4 from school 2023/3/1

--- TaiNanEEITLabSrc(20230206)/EEIT61 11陳逸昇/homework4.js
+++ TaiNanEEITLabSrc(20230206)/EEIT61 11陳逸昇/homework4.js
@@ -1,6 +1,25 @@
+let pictureArray = [{ img: "Images/1676339879809.jpg" }, { img: "Images/1676827015355.jpg" }, { img: "Images/1677400120062.jpg" }];
 
-function currentSlide(pictureNum) {
-    document.querySelectorAll("img")[pictureNum - 1]
+
+window.setInterval(slide, 3000)
+
+console.log(document.querySelectorAll("span")[0].id)
+
+function currentSlide(buttonCount) {
+    let content = document.getElementById("#content");
+    content.firstElementChild.innerHTML = `<img src =${pictureArray[buttonCount - 1].img}>`
+    document.getElementById(`dot${buttonCount}`).className = "clicked"
 }
 
-
+let count = 0;
+function slide(event) {
+    if (count < 3) {
+        let content = document.getElementById("content");
+        content.firstElementChild.innerHTML = `<img src =${pictureArray[count].img}>`
+        count++
+    }else if(count == 3) {
+        count=0
+        let content = document.getElementById("content");
+        content.firstElementChild.innerHTML = `<img src =${pictureArray[count].img}>`
+    }
+}
